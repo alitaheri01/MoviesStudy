fc6c4ce0ec4e3f80d291c0cf8ddd31fa157b383e
Ali Taheri <103609863+alitaheri01@users.noreply.github.com>
2022-05-15 13:43:45 -0700

    Update Movies_v01.py

--- Movies_v01.py
+++ Movies_v01.py
@@ -14,11 +14,36 @@ from matplotlib.pyplot import figure
 %matplotlib inline
 matplotlib.rcParams['figure.figsize'] = (12,8) # Adjust the configurations of the plots we will create
 
+
+
+
 # Read in the data
 
 df = pd.read_csv(r' movies.csv)
+         
+                 
+                 
                  
 # Let's look at the data
 
 df.head()                 
 
+
+                 
+# Let's see if there is any missing data
+
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col,pct_missing)) 
+    
+
+                 
+                 
+# Data types for our columns
+
+df.dtypes
+                 
+                 
+                 
+                 
+                 
