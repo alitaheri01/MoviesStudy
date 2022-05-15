953eb9bbfc5ef10c82feb482d85d29a73eea5b66
Ali Taheri <103609863+alitaheri01@users.noreply.github.com>
2022-05-15 13:33:39 -0700

    Create Movies_v01.py
    Initial  Commit

---
+++ Movies_v01.py
@@ -0,0 +1,20 @@
+# First let's import the packages we will use in this project
+# You can do this all now or as you need them
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib
+
+plt.style.use('ggplot')
+from matplotlib.pyplot import figure
+
+%matplotlib inline
+matplotlib.rcParams['figure.figsize'] = (12,8) # Adjust the configurations of the plots we will create
+
+# Read in the data
+
+df = pd.read_csv(r'/Users/ali/movies.csv')
+
